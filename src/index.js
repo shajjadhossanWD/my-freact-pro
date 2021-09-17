18ce2417b4c7e2fd4744ebed7ae9ef1dd76dd7cd
shajjadhossanWD <shajjadhossan111@gmail.com>
2021-09-17 21:46:23 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const myFname = "Shajjad";
+const myLname = "Hossan"
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+const image1 = "https://picsum.photos/200/300";
+const image2 = "https://picsum.photos/250/300";
+const image3 = "https://picsum.photos/300/300";
+const location = "https://picsum.photos/";
+
+
+
+let curDate = new Date();
+curDate = curDate.getHours();
+let updateData = '';
+let cssStyle = {};
+if(curDate >=5 && curDate <= 12 ){
+  updateData = "good morning";
+  cssStyle.color = "aqua";
+}else if(curDate > 12 && curDate <= 16) {
+  updateData = "good afternoon";
+  cssStyle.color = " yellow";
+}else if(curDate > 16 && curDate <= 18){
+  updateData = " good evening";
+  cssStyle.color = "blue";
+}else{
+  updateData = "good Night";
+  cssStyle.color = "black";
+}
+
+const paragraph = {
+  color: 'red',
+  textTransform: 'uppercase',
+  textAlign: 'center'
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1 className = "heading">{`${myFname} ${myLname}`} the front End Web Developer:)</h1>
+  <p style = {paragraph}>current Date is = {currentDate} </p>
+  <p style = {paragraph}>current time is = {currentTime}</p>
+  <div>
+  <h2> Hello Sir,<span style={cssStyle}> {updateData}</span> </h2>
+  </div>
+  </>,
+  // we can use <></>,[], or any colosing element such as <div></div>
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
